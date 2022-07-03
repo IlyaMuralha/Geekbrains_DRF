84bf23c9f21bbb38388056a5026443827c3ced95
IlyaMuralha <muralhakuzmenko@gmail.com>
2022-07-03 21:28:02 +0500

    Added authtorizations with cookie

--- todo_service/frontend/src/App.js
+++ todo_service/frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import {Container, Nav, Navbar} from "react-bootstrap";
 import axios from "axios";
+import Cookies from "universal-cookie/es6";
 import LoginForm from "./components/auth/Auth";
 import UserList from "./components/todousers/TodoUser";
 import Footer from "./components/footer/Footer";
@@ -22,30 +23,81 @@ class App extends Component {
         'todousers': [],
         'projects': [],
         'todoes': [],
+        'token': '',
+    }
+
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+
+    is_authenticated() {
+        return this.state.token !== ''
+    }
+
+    logout() {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+
+
+    get_token(username, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/', {
+            username: username,
+            password: password
+        })
+            .then(response => {
+                this.set_token(response.data['token'])
+            }).catch(error => alert('Неверный логин или пароль'))
+    }
+
+    get_headers() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_authenticated()) {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
     }
 
 
     load_data = () => {
-        axios.get('http://127.0.0.1:8000/api/project/')
+        const headers = this.get_headers()
+
+        axios.get('http://127.0.0.1:8000/api/project/', {headers})
             .then(response => {
-                console.log(response.data.results)
                 this.setState({projects: response.data.results})
-            }).catch(error => console.log(error))
+            }).catch(error => {
+            console.log(error)
+            this.setState({'project': []})
+        })
 
-        axios.get('http://127.0.0.1:8000/api/todousers/')
+        axios.get('http://127.0.0.1:8000/api/todousers/', {headers})
             .then(response => {
-                console.log(response.data.results)
                 this.setState({todousers: response.data.results})
-            }).catch(error => console.log(error))
+            }).catch(error => {
+            console.log(error)
+            this.setState({'todousers': []})
+        })
 
-        axios.get('http://127.0.0.1:8000/api/todo/')
+        axios.get('http://127.0.0.1:8000/api/todo/', {headers})
             .then(response => {
-                console.log(response.data.results)
                 this.setState({todoes: response.data.results})
-            }).catch(error => console.log(error))
+            }).catch(error => {
+            console.log(error)
+            this.setState({'todoes': []})
+        })
     }
 
     componentDidMount() {
+        this.get_token_from_storage()
         this.load_data()
     }
 
@@ -67,7 +119,8 @@ class App extends Component {
                                     <Link to='/todo'>ToDoes</Link>
                                 </Nav.Link>
                                 <Nav.Link href='/login'>
-                                    <Link to='/login'>Login</Link>
+                                    {this.is_authenticated() ? <button
+                                        onClick={() => this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
                                 </Nav.Link>
                             </Nav>
                         </Container>
@@ -76,7 +129,8 @@ class App extends Component {
                         <Route exact path='/' component={() => <UserList todousers={this.state.todousers}/>}/>
                         <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects}/>}/>
                         <Route exact path='/todo' component={() => <ToDoList todoes={this.state.todoes}/>}/>
-                        <Route exact path='/login' component={() => <LoginForm/>}/>
+                        <Route exact path='/login' component={() => <LoginForm
+                            get_token={(username, password) => this.get_token(username, password)}/>}/>
                         <Route component={NotFound404}/>
                     </Switch>
                     <Footer/>
--- todo_service/todo_service/settings.py
+++ todo_service/todo_service/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.DjangoModelPermissions'
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
